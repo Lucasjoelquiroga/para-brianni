e68cf098706e939fcf4555c86fb03fbd3462a2a0
lucas joel quiroga <lucasjoelquiroga@gmail.com>
2022-07-31 06:44:55 -0300

    hola

--- src/assets/import.js
+++ src/assets/import.js
@@ -4,6 +4,15 @@ import src3 from '../assets/imagenes/frozen.png'
 import src4 from '../assets/imagenes/isabella.png'
 import src5 from '../assets/imagenes/jazmin.png'
 import src6 from '../assets/imagenes/miravel.png'
+import src7 from '../assets/imagenes/aurora.png'
+import src8 from '../assets/imagenes/blancanieves.png'
+import src9 from '../assets/imagenes/cenicienta.png'
+import src10 from '../assets/imagenes/mulan2.png'
+import src11 from '../assets/imagenes/rapunzel.png'
+import src12 from '../assets/imagenes/tiana.png'
+import src13 from '../assets/imagenes/ana.png'
+import src14 from '../assets/imagenes/olaf.png'
+import src15 from '../assets/imagenes/stich.png'
 
 
 const images = [
@@ -56,6 +65,78 @@ const images = [
     {
         src: src6,
         player: 'miravel'
+    },
+    {
+        src: src7,
+        player: 'aurora'
+    },
+    {
+        src: src7,
+        player: 'aurora'
+    },
+    {
+        src: src8,
+        player: 'blancanieves'
+    },
+    {
+        src: src8,
+        player: 'blancanieves'
+    },
+    {
+        src: src9,
+        player: 'cenicienta'
+    },
+    {
+        src: src9,
+        player: 'cenicienta'
+    },
+    {
+        src: src10,
+        player: 'mulan2'
+    },
+    {
+        src: src10,
+        player: 'mulan2'
+    },
+    {
+        src: src11,
+        player: 'rapunzel'
+    },
+    {
+        src: src11,
+        player: 'rapunzel'
+    },
+    {
+        src: src12,
+        player: 'tiana'
+    },
+    {
+        src: src12,
+        player: 'tiana'
+    },
+    {
+        src: src13,
+        player: 'ana'
+    },
+    {
+        src: src13,
+        player: 'ana'
+    },
+    {
+        src: src14,
+        player: 'olaf'
+    },
+    {
+        src: src14,
+        player: 'olaf'
+    },
+    {
+        src: src15,
+        player: 'stich'
+    },
+    {
+        src: src15,
+        player: 'stich'
     },
 
 ]
